4475292b53f1b0b48d56c60d27005de83c3e9354
tanneeru vamshi krishna <35188602+tanneeru-vamshi-krishna@users.noreply.github.com>
2018-06-06 18:20:13 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const vamshi = require('./vamshi.json');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(vamshi))
 
 app.listen(3000
 , () => console.log('Example app listening on port 3000!'))
